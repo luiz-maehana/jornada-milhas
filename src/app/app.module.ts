20de18ae18c68222b2193c13cea4735fbf80d278
Luiz Fernando Maehana <luizmaehana@gmail.com>
2024-03-04 20:38:14 -0300

    Banner + Container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CardComponent } from './shared/card/card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { ContainerComponent } from './shared/container/container.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
     FooterComponent,
     BannerComponent,
     CardComponent,
+    ContainerComponent,
   ],
   imports: [
     BrowserModule,
