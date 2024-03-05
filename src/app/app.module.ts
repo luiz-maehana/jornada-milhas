decc5d384cacee9ad89d2f8e1bf03380eb6245ba
Luiz Fernando Maehana <luizmaehana@gmail.com>
2024-03-04 21:03:27 -0300

    Card Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './shared/header/header.component';
+import { ContainerComponent } from './shared/container/container.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BannerComponent } from './shared/banner/banner.component';
 import { CardComponent } from './shared/card/card.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { ContainerComponent } from './shared/container/container.component';
+import { MatCardModule } from '@angular/material/card';
+
 import { HomeComponent } from './pages/home/home.component';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
+import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
+
 
 @NgModule({
   declarations: [
@@ -22,6 +30,9 @@ import { HomeComponent } from './pages/home/home.component';
     CardComponent,
     ContainerComponent,
     HomeComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent,
+    FormBuscaComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +40,7 @@ import { HomeComponent } from './pages/home/home.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
